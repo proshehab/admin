da88ede8e67ab2b2ab697316618dc92a8356036e
Shehab Bin  Shakib <96295133+proshehab@users.noreply.github.com>
2026-05-21 17:05:31 +0600

    feat: implement Vue Router for navigation and create Login component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import { createPinia } from "pinia";
+import router from "./router";
 
-createApp(App).mount("#app");
+import App from "./App.vue";
+import "./style.css";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
---
+++ src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="min-h-screen flex justify-center items-center bg-base-200">
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <div class="card-body">
+        <h2 class="text-center text-3xl font-bold">Admin Login</h2>
+
+        <input
+          type="email"
+          placeholder="Email"
+          class="input input-bordered w-full"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          class="input input-bordered w-full mt-3"
+        />
+
+        <button class="btn btn-primary mt-4 w-full" @click="login">
+          Login
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const login = () => {
+  router.push("/dashboard");
+};
+</script>
