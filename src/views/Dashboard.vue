bd9c3086b6b7e2aad20aec542eaf9b01c7487e19
Shehab Bin  Shakib <96295133+proshehab@users.noreply.github.com>
2026-05-25 16:42:19 +0600

    feat: create Sidebar component and integrate it into the Dashboard view

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,44 +1,6 @@
 <template>
   <div class="min-h-screen flex bg-gray-100">
-    <!-- SIDEBAR -->
-    <aside class="w-64 bg-gray-900 text-white hidden lg:block">
-      <div class="p-5 text-2xl font-bold border-b border-gray-700">
-        Admin Panel
-      </div>
-
-      <nav class="p-4 space-y-2">
-        <RouterLink
-          to="/dashboard"
-          class="flex items-center gap-3 px-4 py-2 rounded-lg bg-gray-800"
-        >
-          <LayoutDashboard class="w-5 h-5" />
-          Dashboard
-        </RouterLink>
-
-        <RouterLink
-          to="/users/create"
-          class="flex items-center gap-3 px-4 py-2 rounded-lg hover:bg-gray-800"
-        >
-          <Users class="w-5 h-5" />
-          Users
-        </RouterLink>
-
-        <a
-          class="flex items-center gap-3 px-4 py-2 rounded-lg hover:bg-gray-800"
-        >
-          <ShoppingCart class="w-5 h-5" />
-          Orders
-        </a>
-
-        <a
-          class="flex items-center gap-3 px-4 py-2 rounded-lg hover:bg-gray-800"
-        >
-          <BarChart3 class="w-5 h-5" />
-          Reports
-        </a>
-      </nav>
-    </aside>
-
+    <Sidebar />
     <!-- MAIN -->
     <div class="flex-1 flex flex-col">
       <!-- TOP NAV -->
@@ -107,4 +69,5 @@ import {
   LogOut,
   Wallet,
 } from "lucide-vue-next";
+import Sidebar from "../components/Sidebar.vue";
 </script>
